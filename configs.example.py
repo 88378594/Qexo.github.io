d346c8655afe66e60d6a2f462acb9938de3278d9
余生云工作室 <57458261+88378594@users.noreply.github.com>
2025-03-29 20:23:01 +0800

    ss
    ss

--- configs.example.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-DOMAINS = ["*"]
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': Path(__file__).parent / 'db' / 'db.sqlite3',
-    }
-}
--- vercel.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Install dependencies
-python3 -m pip install -r requirements.txt
-
-# Migrate database
-python3 manage.py makemigrations
-python3 manage.py migrate
